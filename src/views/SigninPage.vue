a4f27ff38dd9c60b2f2f9add2ca60bf014cec29a
PleBea <bini060315kim@gmail.com>
2022-06-16 01:14:20 +0900

    create: SingInPage

--- src/views/SigninPage.vue
+++ src/views/SigninPage.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>hi</div>
+    <div id="register-box">
+        <h1>Sign In</h1>
+        <div class="account-input-box">
+            <input type="text" placeholder="id">
+            <input type="text" placeholder="email">
+            <hr style="margin:3px 0 3px 0;">
+            <input type="password" placeholder="pw">
+            <input type="password" placeholder="pw check">
+        </div>
+        <button class="account-btn">Sign In</button>
+    </div>
 </template>
 
 <script>
@@ -9,5 +19,17 @@ export default {
 </script>
 
 <style>
+#register-box {
+    transform: translate(-50%, -70%);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+}
+#register-box h1 {
+    font-family: inherit;
+    text-align: center;
+    font-size: 36px;
 
+    margin-bottom: 14px;
+}
 </style>
